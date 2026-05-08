36713cc2e3e60116f66dab1da4ec0584e9888f89
Sambala98 <s_ambala@u.pacific.edu>
2026-05-08 00:04:07 -0700

    Connect Vercel frontend to Render backend

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://order-management-api.vercel.app",,
+        "https://order-management-api-eta.vercel.app",
         ],
     allow_credentials=True,
     allow_methods=["*"],
