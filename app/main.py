5672280d4adf89fbd1d32b01db2aab9af3b2d3d7
Sambala98 <s_ambala@u.pacific.edu>
2026-05-07 23:59:55 -0700

    Connect frontend to Render backend

--- app/main.py
+++ app/main.py
@@ -16,7 +16,10 @@ app = FastAPI(title=settings.APP_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://order-management-api.vercel.app",,
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
